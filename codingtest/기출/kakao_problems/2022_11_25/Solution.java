1d417a6755ef1c779a49f3fa4f9574ff437dfb22
Hanjione <gkswldnj0803@gmail.com>
2022-11-25 22:52:24 +0900

    카카오 모빌리티 대비 기출 3제완료

---
+++ codingtest/기출/kakao_problems/2022_11_25/Solution.java
@@ -0,0 +1,14 @@
+package kakao_problems;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+//        s.solution();
+//        String binary = Integer.toBinaryString(9);
+//        System.out.println(binary);
+
+
+    }
+}
--- codingtest/기출/kakao_problems/2022_11_25/Solution2.java
+++ codingtest/기출/kakao_problems/2022_11_25/Solution2.java
@@ -1,92 +1,104 @@
 package kakao_problems;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+
 /*
 프로그래머스 lv 2 뉴스클러스터링 https://school.programmers.co.kr/learn/courses/30/lessons/17677
-시도 X
+시도 X O
  */
 public class Solution2 {
-    String[] overlap, union;
-    String [] asso1, asso2;
+    List<String> array1, array2; //다중 집합을 담을 리스트
+    ArrayList<String> intersection, union; //교집합과 합집합
 
     public int solution(String str1, String str2) {
-        //배열들 초기화
         int answer = 0;
-        overlap= new String[0];
-        union = new String[0];
-        asso1 = makeManySet(str1);
-        asso2 = makeManySet(str2);
-        makeOverlapUnion();
-        return (int) getAnswer(); //**check
+
+        //배열들 초기화
+        intersection = new ArrayList<>();
+        union = new ArrayList<>();
+
+        //1. 다중집합의 원소로 만든다.
+        array1 = new ArrayList<>(makeOverlapUnion(str1));
+        array2 = new ArrayList<>(makeOverlapUnion(str2));
+
+        Collections.sort(array1);
+        Collections.sort(array2);
+
+        //2. 합집합과 교집합을 구한다.
+        getUionIntersection();
+
+        //3. 자키드 유사도 출력
+        double result = getJacckad();
+        return (int)result ;
     }
 
-    public boolean isInOtherString(String str){
-        if ( str.matches(".*[0-9].*") ) return true;
+    private void getUionIntersection() {
+        //오름차순 정렬을 해주어야
+
+        for (String s : array1) {
+            if (array2.remove(s)) {
+                intersection.add(s);
+            }
+            union.add(s);
+        }
+        union.addAll(array2);
+    }
+
+    public boolean isInOtherString(String str) {
+        if (str.matches(".*[0-9].*")) return true;
 
         //문자열에 공백 혹은 특수문자가 입력된 경우
         String pattern = "^[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$";
-        if(!Pattern.matches(pattern, str)){
+        if (!Pattern.matches(pattern, str)) {
             return true;
-//            System.out.println("공백 혹은 특수문자가 입력되었습니다.");
         }
         return false;
     }
 
 
     // 두 글자씩 다중 집합을 만들기
-    public String [] makeManySet(String str){
-        String [] arr = new String[str.length()-1]; //분할 하면 생기는 갯수 = str.length()-1
-        for (int i=0; i<str.length()-1;i++){
-            if (!isInOtherString( str.substring(i,i+2) )) { //특수문자, 공백, 숫자 없다면
-                arr[i]= str.substring(i,i+2);
+    public List<String> makeOverlapUnion(String str) {
+        ArrayList<String> array = new ArrayList<>();
+
+        //우선 먼저 다 대문자로 만들어준다.
+        str = str.toUpperCase();
+
+        for (int i = 0; i < str.length() - 1; i++) {
+
+            if (!isInOtherString(str.substring(i,i+2))){
+                array.add(str.substring(i,i+2));
             }
 
         }
-        return arr;
+
+//        for (int i = 0; i < str.length() - 1; i++) {
+//            char a = str.charAt(i);
+//            char b = str.charAt(i + 1);
+//            if (Character.isLetter(a) && Character.isLetter(b)) { //모두 문자가 맞다면
+//                StringBuilder sb = new StringBuilder();
+//                sb.append(a).append(b);
+//                array.add(new String(sb));
+//
+//            }
+//        }
+        return array;
     }
 
-    //교집합과 합집합 구하기
-    public void makeOverlapUnion(){
-        for (int i=0; i<asso1.length; i++){
-            for (int j =0;j<asso2.length; j++){
-                //대문자, 소문자 차이는 무시.
-                if ( asso1[i].equalsIgnoreCase( asso2[j] ) ){
-                    overlap[overlap.length] = asso1[i];
-
-                }
-                union[union.length] = asso1[i];
-                union[union.length] = asso1[j];//check -?? union.length+1??
-            }
-        }
-        //중복 제거 https://developer-talk.tistory.com/453
-        List<String> newUnion = Arrays.asList(union)
-                .stream()
-                .distinct()
-                .collect(Collectors.toList());
-        List<String> newOverlap = Arrays.asList(overlap)
-                .stream()
-                .distinct()
-                .collect(Collectors.toList());
-
-//        System.out.println(Collections.unmodifiableList(newUnion));
-//        System.out.println(Collections.unmodifiableList(newOverlap));
-
-    }
-
-    public double getAnswer() {
-        double d = overlap.length / union.length;
+    public double getJacckad() {
+        double d =0;
+        if (union.size()==0) d=1;
+        else d = (double)intersection.size() / (double)union.size();
         d = d * 65536;
-        return Math.floor(d);
+//        return Math.floor(d);
+        return d;
     }
 
     public static void main(String[] args) {
         Solution2 s = new Solution2();
-        s.solution("FRANCE", "french");
-
-
+//        System.out.println(s.solution("FRANCE", "french"));
+        System.out.println(s.solution("E=M*C^2", "e=m*c^2"));
     }
 }
