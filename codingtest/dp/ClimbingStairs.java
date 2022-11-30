e612e0b92960e8d31bbda6be55a901818e10056e
Hanjione <gkswldnj0803@gmail.com>
2022-11-30 17:01:37 +0900

    계단 오르기 복습 완료 git add .

--- codingtest/dp/ClimbingStairs.java
+++ codingtest/dp/ClimbingStairs.java
@@ -1,63 +1,86 @@
 package dp;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
 /*
 실버3 계단 오르기 https://www.acmicpc.net/problem/2579
-** 생각하기 어려움 다시 꼭 풀어볼 것.
+심화 : 경로 출력 -> bfs 사용 X , more 클린 코드
+시도 : O
  */
 public class ClimbingStairs {
 
-    static int N;//계단의 수
-    static int[][] Dy;
+    static class From{
+        int idx, depth;
 
-    static int [] A;
-
-    static void input() {
-        FastReader sc = new FastReader();
-
-        N = sc.nextInt();
-        Dy = new int[N +1][2];
-        A = new int[N +1];
-        for (int i=1; i<= N; i++){
-            A[i] = sc.nextInt();
+        public From(int idx, int depth) {
+            this.idx = idx;
+            this.depth = depth;
         }
-
-        sc.close();//스트림을 닫아 종료된 작업에 대한 메모리 확보.
-
     }
 
+    static FastReader sc = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int [] A;
+    static int[][] Dy;
+    //심화
+    static From [][] From;
 
+    static void input(){
+        N= sc.nextInt();
+        A = new int[N+1];
+        Dy = new int[N+1][2];
+        From = new From[N+1][2];
+        for (int i=1; i<=N; i++ ){
+            A[i] = sc.nextInt();
+        }
+    }
     static void preprocess() {
-        //초기값 설정
 
-        //*문제 조건에 의하여 0은 어짜피 올 수 없으며, 무조건 1이상이다.
-        Dy[1][0]=0;
-        Dy[1][1]=A[1];
+        Dy[1][0]=A[1];
+        Dy[1][1]=0;
 
-        if (N >= 2){ //** 주의 , if문을 해주지 않으면 N=1일때 에러 발생.
-            Dy[2][0] = A[2];
-            Dy[2][1] = A[1] + A[2];
+        From[1][0] = new From(0,0);
+        From[1][1] = new From(0,0);
+
+        if (N >=2){
+            Dy[2][0]=A[2];
+            Dy[2][1]=Dy[1][0]+ A[2];
+
+            From[2][0] = new From(0,0);
+            From[2][1] = new From(1,0);
+        }
+        for (int i=3; i<=N; i++){
+            Dy[i][0] = Math.max(Dy[i-2][0], Dy[i-2][1] )+ A[i];
+            Dy[i][1] = Dy[i-1][0]+ A[i];
+
+            /* 심화, 경로구하기 */
+            From[i][0] = new From(i-2, Dy[i-2][0] > Dy[i-2][1] ? 0: 1);
+            From[i][1] = new From(i-1,0);
+
         }
 
-        //값 채워 넣기
-        for (int i=3;i<=N; i++){
-            Dy[i][0] = Math.max( Dy[i-2][0], Dy[i-2][1]) + A[i];
-            Dy[i][1]= Dy[i-1][0] + A[i];
+    }
+    static void pro(){
+        input();
+        preprocess();
+        System.out.println(Math.max(Dy[N][0], Dy[N][1]));
+
+        /* 심화, 경로구하기 */
+        From fr = From[N][ Dy[N][0] > Dy[N][1] ? 0:1];
+        while (fr != null){ //From table의 0,0은 null이다.
+            sb.insert(0,fr.idx+" ");
+            fr = From[fr.idx][fr.depth];
         }
+        sb.append(N);
+        System.out.println(sb);
+
     }
 
     public static void main(String[] args) {
-        input();
-        preprocess();
 
-        System.out.println(Math.max(Dy[N][0], Dy[N][1]));
-
+        pro();
+        sc.close();//스트림을 닫아 종료된 작업에 대한 메모리 확보.
     }
-
     static class FastReader {
             BufferedReader br;
             StringTokenizer st;
