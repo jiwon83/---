57dcfddd9d78305bb9f44607229cd4e2940acb21
Hanjione <gkswldnj0803@gmail.com>
2022-12-06 15:51:37 +0900

    성냥 개비 - 실패

---
+++ codingtest/dp/BOJ3687성냥개비.java
@@ -0,0 +1,19 @@
+package dp;
+/*
+네이버 기출 유사 문제 : https://www.acmicpc.net/problem/3687
+참고: http://blog.devjoshua.me/2019/11/30/boj-3687/
+ */
+public class BOJ3687성냥개비 {
+
+
+    static void input() {
+
+    }
+    static void pro() {
+
+    }
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+}
