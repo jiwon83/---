9843dc97de4c48985b2cb321d2aa97e85a4f0e55
Hanjione <gkswldnj0803@gmail.com>
2023-01-19 08:04:49 +0900

    거북이

--- codingtest/simulation/BOJ8911_거북이.java
+++ codingtest/simulation/BOJ8911_거북이.java
@@ -1,67 +1,51 @@
-package simulation;
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class BOJ8911_거북이 {
+public class Main{
     static StringBuilder sb = new StringBuilder();
     static FastReader sc = new FastReader();
-    static int [] dx = {0,1,0,-1};
-    static int [] dy = {1,0,-1,0};
     static String command;
+    static int maxX, minX, maxY, minY, nowDir, nowX, nowY;
+    static int [] diX ={0,1,0,-1}, diY = {1,0,-1,0};
 
     static void input() {
-        int T = sc.nextInt();
-        while (T-- > 0){
-            command= sc.next();
-            pro();
-        }
+        command = sc.next();
     }
-    static void move(char com){
-        //com과 현재 방향에 따라 이동하고, 값들을 갱신.
-        switch (com){
-            case 'F':
-                nowX = nowX + dx[nowDirect];
-                nowY = nowY + dy[nowDirect];
-
-                break;
-            case 'B':
-                nowX = nowX - dx[nowDirect];
-                nowY = nowY - dy[nowDirect];
-
-                break;
-            case 'L':
-                nowDirect = nowDirect-1;
-                if (nowDirect==-1) nowDirect=3;
-
-                break;
-            case 'R':
-                nowDirect = (nowDirect+1) % 4;
-                break;
-        }
-        maxX = Math.max(maxX, nowX);
-        maxY = Math.max(maxY, nowY);
-        minX = Math.min(minX, nowX);
-        minY = Math.min(minY, nowY);
-    }
-    static int maxX, maxY, minX, minY;
-    static int nowX, nowY, nowDirect;
     static void pro() {
-        minX =0; minY=0; maxX=0; maxY=0;
-        nowX =0; nowY=0; nowDirect=0; // 북 0 동 1 남 2 서 4
+        nowX=0; nowY=0; maxX=0; minX=0;minY=0;maxY=0;nowDir=0;
         for (int i=0; i<command.length(); i++){
-            char com = command.charAt(i);
-            move(com);
+            char c = command.charAt(i);
+            switch(c){
+                case 'F':
+                    nowX += diX[nowDir];
+                    nowY += diY[nowDir];
+                    break;
+                case 'B':
+                    nowX -= diX[nowDir];
+                    nowY -= diY[nowDir];
+                    break;
+                case 'L':
+                    nowDir -= 1;
+                    if (nowDir==-1) nowDir=3;
+                    break;
+                case 'R':
+                    nowDir = (nowDir+ 1) % 4;
+                    break;
+            }
+            minX = Math.min(minX, nowX);
+            maxX = Math.max(maxX, nowX);
+            minY = Math.min(minY, nowY);
+            maxY = Math.max(maxY, nowY);
         }
-        int result = (Math.abs(maxY-minY))*(Math.abs(maxX-minX));
-        sb.append(Math.abs(result)).append("\n");
+        System.out.println((maxX - minX) * (maxY - minY));
 
     }
     public static void main(String[] args) {
-        input();
-        System.out.println(sb);
+        int T = sc.nextInt();
+        while (T-- >0 ){
+            input();
+            pro();
+        }
     }
     static class FastReader {
             BufferedReader br;
@@ -109,3 +93,7 @@ public class BOJ8911_거북이 {
 
         }
 }
+
+
+
+
