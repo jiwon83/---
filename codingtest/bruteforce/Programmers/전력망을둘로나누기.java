f2f8431acfb0e163c0a3ea215d211217d1fef92d
Hanjione <gkswldnj0803@gmail.com>
2023-04-09 14:05:14 +0900

    전력망 dfs

--- codingtest/bruteforce/Programmers/전력망을둘로나누기.java
+++ codingtest/bruteforce/Programmers/전력망을둘로나누기.java
@@ -1,99 +1,42 @@
 import java.util.*;
 class Solution {
-    
-    int N;
-    ArrayList<Integer> graph [] ;
-    int [][] Wires;
-    boolean [] visit;
-    
-    int dfs(int x, int [] deleteEdge, int cnt){
-        
-        visit[x] =true;
-        int num=1;
+    boolean visit [];
+    ArrayList<Integer> [] graph;
+    public int dfs(int x, int[] remove){
+
         boolean cango=false;
-        
-        for(int node : graph[x]){
-            if(x==deleteEdge[0] && node == deleteEdge[1]) continue;
-            if(x==deleteEdge[1] && node == deleteEdge[0]) continue;
-            if(visit[node]) continue;
-            cango = true;
-            num += dfs(node, deleteEdge, cnt+1);
-            
+        int cnt=1;
+        for(int next: graph[x]){
+                if(visit[next]) continue;
+                if((x==remove[0] && remove[1] ==next) || (x==remove[1]&& next==remove[0])) continue;
+                cango = true;
+                visit[next]=true;
+                cnt += dfs(next, remove);
         }
-        if(!cango) return num;
-        return num;
-        
+        if(!cango){
+            return 1;
+        }
+        return cnt;
     }
-    
     public int solution(int n, int[][] wires) {
-        N = n;
-        graph =  new ArrayList [N+1];
-        // cnt = new int[N+1];
-        Wires = wires;
-        visit = new boolean[N+1];
-        
-        
-        
-        //graph 초기화
-        for(int i=1; i<=N; i++){
+        int ans =n;
+        graph = new ArrayList[n+1];
+        for(int i=1; i<= n; i++){
             graph[i] = new ArrayList<>();
         }
-            
-        int answer = N;
-        //1. 그래프를 생성
-        for( int i=0; i<wires.length; i++){
+        for(int i=0; i<wires.length; i++){
             int from = wires[i][0];
             int to = wires[i][1];
             graph[from].add(to);
             graph[to].add(from);
-
-            
+        }
+        for(int i=0; i<wires.length; i++){
+            visit = new boolean[n+1];
+            visit[1] = true;
+            int cntGroup1 =  dfs(1, wires[i]);
+            ans = Math.min(ans, Math.abs((n - cntGroup1) - cntGroup1));
         }
         
-        
-        System.out.println(Arrays.toString(graph));
-        
-        
-        for(int i=0; i< wires.length; i++){ //삭제할 노드
-            
-            int [] deleteEdge = wires[i];
-            int [] groups = new int[2];
-            int idx = 0;
-            visit = new boolean[N+1];
-            
-            System.out.println("deleteEdge"+Arrays.toString(deleteEdge));
-            
-            for(int start =1; start<=n; start++){ //그래프 탐색을 시작할 노드
-                
-                if(visit[start]) continue;
-                
-                int num = dfs(start, deleteEdge, 0);
-                System.out.println("start "+start + " 노드 수 "+num);
-                groups[idx++] = num;
-                
-                
-            }
-            
-            System.out.println(Arrays.toString(groups));
-            answer = Math.min(answer, Math.abs( groups[0] - groups[1] ));
-        }
-//         // System.out.println(Arrays.toString(graph));
-//         //2. bfs 로 순회해서 cnt 갱신
-//         for(int i=1; i<=N; i++){
-//             dfs(i,i);
-//         }
-        // System.out.println(Arrays.toString(cnt));
-        
-        //3. cnt + 1중 n/2와 가장 비슷한 지점에서 자른다.
-        //- cnt 값 + 1 과 n- 이 값의 차의 절댓값 반환.
-//         for(int i=1; i<=N; i++){
-//             int d = cnt[i]+1;
-//             int gap = Math.abs( (N - d) - d );
-//             if( gap < answer){
-//                 answer = gap;
-//             }
-            
-//         }
-        return answer;
+        return ans;
     }
 }
