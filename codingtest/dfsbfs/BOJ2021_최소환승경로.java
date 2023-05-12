4088ace1f13f2b45c0f33093ebc17c8bb4fda8fd
Hanjione <gkswldnj0803@gmail.com>
2023-05-12 09:56:26 +0900

    최소 환승 경로 복습

--- codingtest/dfsbfs/BOJ2021_최소환승경로.java
+++ codingtest/dfsbfs/BOJ2021_최소환승경로.java
@@ -1,161 +1,158 @@
 import java.util.*;
 import java.io.*;
-
+/*
+test case
+10 3
+1 2 3 4 5 9 -1
+7 3 10 -1
+7 9 6 3 8 2 -1
+1 10
+*/
 public class Main {
-    static class Edge {
+    static class Info{
         int node, line, transCnt;
-
-        public Edge(int node, int line, int transCnt) {
+        public Info(int node, int line, int transCnt){
             this.node = node;
             this.line = line;
             this.transCnt = transCnt;
         }
     }
-
     static StringBuilder sb = new StringBuilder();
     static FastReader sc = new FastReader();
-    static int N, L, S, E;
-    static ArrayList<Integer>[] lines;// 각 라인에 있는 스테이션 정보
-    static ArrayList<Integer>[] stations;// 각 스테이션의 Line 정보
-    static boolean[] visitLine;
-    static boolean[] visitStation;
-    static int ans=-1;
+    static ArrayList<Integer> [] linesOfNode;
+    static ArrayList<Integer> [] nodesOfLine;
+    static boolean [] visitNode, visitLine;
+    static int N, L, S, E, ans;
+    static int [] memo;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     static void input() {
-/*
-10 3
-1 2 3 4 5 -1
-9 7 10 -1
-7 6 3 8 -1
-1 10
- */
         N = sc.nextInt();
         L = sc.nextInt();
-        stations = new ArrayList[L + 1];
-        lines = new ArrayList[N + 1];
-        visitLine = new boolean[L + 1];
-        visitStation = new boolean[N + 1];
-        for (int i = 1; i <= N; i++) {
-            lines[i] = new ArrayList<>();
-        }
-        for (int i = 1; i <= L; i++) {
+        ans = L;
+        linesOfNode = new ArrayList[N+1];
+        nodesOfLine = new ArrayList[L+1];
+        visitNode = new boolean[N+1];
+        visitLine = new boolean[L+1];
+        memo = new int[L+1];
+        for (int i=1; i<=N; i++) linesOfNode[i] = new ArrayList<>();
+        for (int i = 1; i <= L; i++) nodesOfLine[i] = new ArrayList<>();
 
-            stations[i] = new ArrayList<>();
-            String[] arr = sc.nextLine().split(" ");
-            for (int j = 0; j < arr.length - 1; j++) {
-                int station = Integer.parseInt(arr[j]);
-                lines[station].add(i);
-                stations[i].add(station);
+        for (int i=1; i<=L; i++){
+            String [] arr = sc.nextLine().split(" ");
+            for (int j=0; j<arr.length-1; j++){
+                int n = Integer.parseInt(arr[j]);
+                nodesOfLine[i].add(n);
+                linesOfNode[n].add(i);
+
 
             }
         }
-
         S = sc.nextInt();
         E = sc.nextInt();
+
     }
-
-    static void bfs() {
-        Queue<Edge> q = new LinkedList<>();
-        visitStation[S] = true;
-        for (int line : lines[S]) {
-            q.add(new Edge(S, line, 0));
-            visitLine[line] = true;
-        }
-
-        while (!q.isEmpty()) {
-            Edge edge = q.poll();
-            if (edge.node == E) {
-                ans = edge.transCnt;
-                break;
+    static void recur(int node, int line, int transCnt){
+        //System.out.println("node = " + node + ", line = " + line + ", transCnt = " + transCnt);
+        if (node==E) ans = Math.min(ans, transCnt);
+        else{
+            for (int n : nodesOfLine[line]){
+                if (!visitNode[n]){
+                    visitNode[n] = true;
+                    recur(n, line, transCnt);
+                    visitNode[n] = false;
+                }
             }
-            for (int node: stations[edge.line]){
-                if (visitStation[node]) continue;
-                visitStation[node] = true;
-                q.add(new Edge(node, edge.line, edge.transCnt));
-            }
-            for (int nextLine : lines[edge.node]){
-                if (visitLine[nextLine]) continue;
-                visitLine[nextLine] = true;
-                q.add(new Edge(edge.node, nextLine, edge.transCnt+1));
-            }
+            for (int l : linesOfNode[node]){
 
+                if ( !visitLine[l] || memo[l] >= transCnt+1 ) {
+                    visitLine[l] = true;
+                    memo[l] = transCnt+1;
+                    recur(node, l, transCnt+1);
+                    //visitLine[l] = false;
+                }
+            }
         }
     }
-    static void dfs(int node, int line, int count){
-        if (node==E){
-            ans = Math.min(ans,count);
-            return;
-        }
-        for (int next : stations[line]){
-            if (visitStation[next]) continue;
-            visitStation[next] = true;
-            dfs(next, line, count);
-        }
-        for (int nextLine : lines[node]){
-            if (visitLine[nextLine]) continue;
-            visitLine[nextLine] = true;
-            dfs(node, nextLine, count+1);
-        }
+    static void pro() {
+        System.out.println(bfs());
     }
+    static int bfs(){
+        Queue<Info> q = new LinkedList<>();
+        for (int l : linesOfNode[S]){
+            visitNode = new boolean[N+1];
+            visitLine = new boolean[L+1];
+            visitNode[S] = true;
+            visitLine[l] = true;
+            q.add(new Info(S, l,0));
+        }
+        while (!q.isEmpty()){
+            Info info = q.poll();
+            if (info.node==E) return info.transCnt;
+            //같은 라인 방문
+            for (int n : nodesOfLine[info.line]){
+                if (visitNode[n]) continue;
+                visitNode[n] = true;
+                q.add(new Info(n, info.line, info.transCnt));
+            }
+            //환승
+            for ( int l :linesOfNode[info.node]){
+                if (visitLine[l]) continue;
+                visitLine[l] = true;
+                q.add(new Info(info.node, l, info.transCnt+1));
+            }
+            //환승
+        }
+        return -1;
 
+    }
     public static void main(String[] args) {
         input();
-        bfs();
-        if (ans == Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(ans);
-
+        pro();
     }
-
     static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
+            BufferedReader br;
+            StringTokenizer st;
 
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
+            public FastReader() {
+                br = new BufferedReader(new InputStreamReader(System.in));
 
-        }
+            }
+            String next(){
+                while (st == null || !st.hasMoreTokens()){  //현재 남아 있는 토큰이 없다면 새로 받아온다.
+                    try {
+                        st = new StringTokenizer(br.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return st.nextToken();
+            }
 
-        String next() {
-            while (st == null || !st.hasMoreTokens()) {  //현재 남아 있는 토큰이 없다면 새로 받아온다.
+            int nextInt(){
+                return Integer.parseInt(next());
+            }
+            long nextLong(){return Long.parseLong(next()); }
+
+            double nextDouble(){return Double.parseDouble(next());}
+
+            String nextLine(){
+                String str ="";
                 try {
-                    st = new StringTokenizer(br.readLine());
+                    str = br.readLine();
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                return str;
+            }
+            void close() {
+                try {
+                    br.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            return st.nextToken();
+
         }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-
-        void close() {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
 }
