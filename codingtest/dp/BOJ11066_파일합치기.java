c510d1f239898a5a331cfe64c79013920eed8265
Hanjione <gkswldnj0803@gmail.com>
2023-06-13 11:11:23 +0900

    파일합치기 성공

--- codingtest/dp/BOJ11066_파일합치기.java
+++ codingtest/dp/BOJ11066_파일합치기.java
@@ -1,57 +1,115 @@
-import java.util.*;
 import java.io.*;
-class Main {
-    static int [][] dp, sum;
-    static int [] arr ;
-    static int N;
-    static BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static int K;
+    static int [][] dp;
+    static int [][] fileSize;
+    static int [] a;
     static void input() throws IOException{
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N+1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=1; i<=N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+/*
+2
+4
+40 30 30 50
+15
+1 21 3 4 5 35 5 4 3 5 98 21 14 17 32
+
+ */
+        K = scan.nextInt();
+         a = new int[K+1];
+         fileSize = new int [K+1][K+1];
+         dp =  new int [K+1][K+1]; //필요한 최소 비용
+
+        for (int i=1; i<=K; i++){
+            a[i] = scan.nextInt();
         }
-        dp = new int[N+1][N+1];
-        sum = new int[N+1][N+1];
+
     }
-    static void pro(){
-
-        //초기값
-        for(int i=1; i<=N; i++){
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
-        }
-        //초기값 길이 1
-        for(int i=1; i<=N; i++){
-            dp[i][i]= arr[i];
-
-        }
-        //초기값 길이 2
-        for (int s=1; s+2-1 <= N; s++){
-            dp[s][s+1] = dp[s][s]+dp[s+1][s+1];
-            sum[s][s+1] =dp[s][s+1];
-        }
-
-        //dp 채우기
-        for (int l=3; l<=N; l++){
-            for (int s=1; s+l-1 <= N; s++){
-                int e = s+l-1;
-
-                for (int sp = s; sp < e; sp++){
-                      dp[s][e] = Math.min(dp[s][e], dp[s][sp]+ sum[s][sp] + dp[sp+1][e]+ sum[sp+1][e]);
-                      sum[s][e] =  dp[s][e];
-
-                }
+    static void initFileSize(){
+        for (int i=1; i<=K; i++){
+            fileSize[i][i] = a[i];
+            for (int j=i+1; j<=K; j++){
+                dp[i][j] = Integer.MAX_VALUE;
+                fileSize[i][j] = fileSize[i][j-1] + a[j];
             }
         }
-        for (int i=1; i<=N; i++) System.out.println(Arrays.toString(dp[i]));
-        System.out.println(dp[1][N]);
+//        System.out.println("-----fileSize-----");
+//        System.out.println(Arrays.deepToString(fileSize));
+   }
+    public static void doDp(){
+        //1.
+        initFileSize();
+        //2.
+        for (int len = 2; len <=K; len++){
+            for (int i=1; i + len -1<=K; i++){
+                int j = i+len-1;
+                for (int sp = i; sp<j; sp++){
+                    dp[i][j] = Math.min(dp[i][j], dp[i][sp] + dp[sp+1][j] + fileSize[i][j]);
+                }
+
+            }
+        }
+        System.out.println(dp[1][K]);
     }
+
+
+
     public static void main(String[] args) throws IOException{
-        int T= Integer.parseInt(br.readLine());
-        for(int t=1; t<=T; t++){
+        int T = scan.nextInt();
+        for (int i=0; i<T; i++){
             input();
-            pro();
+            doDp();
+        }
+
+
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 }
