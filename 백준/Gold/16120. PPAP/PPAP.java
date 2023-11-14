dc9e05e65ee61f5820dca85a027e12c18059e13b
한지원 <88698607+jiwon83@users.noreply.github.com>
2023-11-14 09:06:12 +0900

    [Gold IV] Title: PPAP, Time: 256 ms, Memory: 20040 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.java
+++ 백준/Gold/16120. PPAP/PPAP.java
@@ -1,80 +1,86 @@
-import java.io.BufferedReader;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
 
 public class Main {
         static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         static StringBuilder ans = new StringBuilder();
-        static int N, M;
+
+        /**
+         * P PPAP AP
+         * P PPAPPAP AP
+         * PPAPPAP
+         * PP >=2이면 A P를 확인
+         * 있다면 P를 하나 minus
+         * 없다면 P 추가
+         * @param args
+         * @throws IOException
+         */
 
         public static void main(String[] args) throws IOException {
 
 //
                 String input = br.readLine();
-                int [] pos = new int[]{0,0,1,0};
-
-
-                ArrayDeque<Character> queue = new ArrayDeque<>();
-                ArrayDeque<Character> buffer = new ArrayDeque<>();
-                Stack<Character> stack = new Stack<>();
-                String answer = "PPAP";
-
-                int idx = 0;
-                for (int i = 0; i < input.length(); i++) {
-                        stack.push(input.charAt(i));
-                        StringBuilder sb = new StringBuilder();
-
-                        if (stack.size() < 4) continue;
-
-                        for (int j = 0; j < 4; j++) {
-                                sb.append( stack.get(stack.size()-1-j) );
-                        }
-                        if (sb.toString().equals("PAPP")){
-                                int cnt = 0;
-                                while (++cnt <= 3) stack.pop();
-                        }
-                }
-                System.out.println((stack.size()==1 && stack.get(0)=='P')? "PPAP" : "NP");
-
-//                System.out.println(check(input)?"PPAP": "NP");
-                //KMP, O(N), 한 번 돌면서
-                // PPAP
-                // PPPAPAP
-                // PPAP
-                //PPAP PPAP PPAP
-                // P  P P => fail
-                // PPAP PPAP A PPAP => P
-
-
+                System.out.println(check2(input)?"PPAP":"NP");
 
         }
-//        public static boolean check(String s){
-//                String target = "PPAP";//=> p
-//                StringBuilder sb = new StringBuilder();
-//                int pointer = 0;
-//                int idx = 0;
-//                for(int i = 0; i <= s.length(); i++){
-//                        if (target.charAt(idx++)==s.charAt(i)){
-//
-//                                sb.append(s.charAt(i));
-//                                if (idx == 4){
-//
-//                                        sb.setLength(sb.length()-4);
-//                                        sb.append("P");
-//                                        pointer++;
-//                                        idx -=3;
-//
-//                                }
-//                        }
-//                        else return false;
-//
-//                }
-//                return tarI == 4;
-//        }
+        static boolean check2(String s){
+               int cntP = 0;
+               for (int i =0; i<s.length(); i++){
+                       if (cntP >= 2){
+                               if (s.charAt(i)=='A'){
+                                       if(i+1 < s.length() && s.charAt(i+1)=='P'){
+                                               cntP--;
+                                               i++;
+                                       }else{
+                                               return false;
+                                       }
 
+                               }else{
+                                       cntP++;
+                               }
+
+                       }else{
+                               if (s.charAt(i)=='P') cntP++;
+                               else return false;
+                       }
+               }
+               return cntP==1;
+        }
+        static boolean check(String s){
+                String answer = "PPAP";
+                int pointer =0;
+                for (int i = 0; i < s.length(); i++) {
+//                        System.out.println("pointer : "+pointer+ "  i: "+i );
+                        if (answer.charAt(pointer)=='A' && s.charAt(i)!='A'){
+                                return false;
+                        }else if(answer.charAt(pointer) == 'P'){
+                                if(s.charAt(i)!='P') return false;
+                                if (isPPAP(s, i)){
+//                                        System.out.println("PPAP이다..");
+                                        i+= 3;
+                                }
+                        }
+                        pointer++;
+                        if (pointer == 4){
+                                pointer = 0;
+                        }
+
+                }
+                return pointer==0;
+        }
+        static boolean isPPAP(String s, int start){
+                if (start + 3 >= s.length()){
+                        return false;
+                }
+                String answer = "PPAP";
+                for (int j = 0; j < 4; j++) {
+                        if (s.charAt(start+j) != answer.charAt(j)){
+                                return false;
+                        }
+                }
+                return true;
+        }
 
 }
