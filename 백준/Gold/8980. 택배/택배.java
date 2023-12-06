31cce7bbf2592e94d307e0d076eb332368e8c320
한지원 <88698607+jiwon83@users.noreply.github.com>
2023-12-06 12:01:15 +0900

    [Gold I] Title: 택배, Time: 344 ms, Memory: 20788 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.java
+++ 백준/Gold/8980. 택배/택배.java
@@ -1,29 +1,24 @@
 import java.io.BufferedReader;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
-  static class Box implements Comparable<Box>{
-
-    int s, e, count;
-
-    public Box(int s, int e, int count) {
+class Main{
+  static class Box{
+    int s , e , count;
+    public Box(int s, int e, int count){
       this.s = s;
       this.e = e;
       this.count = count;
-    }
-
-    @Override
-    public int compareTo(Box o) {
-      return this.e - o.e;
     }
 
     @Override
@@ -35,102 +30,54 @@ public class Main {
               '}';
     }
   }
-
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static int N, C, M; //M = 박스 정보의 갯수
+  static StringTokenizer st;
+  static int N, K, M;
   static Box [] boxes;
-
-  static void input() throws IOException {
-    StringTokenizer st;
-    st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    C = Integer.parseInt(st.nextToken());
+  static void input() throws IOException{
+    String [] str = br.readLine().split(" ");
+    N = Integer.parseInt(str[0]);
+    K = Integer.parseInt(str[1]);
     M = Integer.parseInt(br.readLine());
     boxes = new Box[M];
-    for (int i = 0; i < M ; i++){
+    for (int i = 0; i< M; i++){
       st = new StringTokenizer(br.readLine());
       int s = Integer.parseInt(st.nextToken());
       int e = Integer.parseInt(st.nextToken());
-      int c = Integer.parseInt(st.nextToken());
-      boxes[i] =new Box(s, e, c);
+      int count = Integer.parseInt(st.nextToken());
+      boxes[i] = new Box(s,e,count);
     }
-
   }
-
-  static void sortBoxes(){
-    // 1. 박스들 출발점, 도착점 오름차순 정렬
-    Arrays.sort(boxes, (o1, o2) -> {
-      if (o1.s != o2.s) return o1.s - o2.s;
-      return o1.e - o2.e;
+  static int sol(int cap, Box [] boxes){
+    int answer = 0;
+    Arrays.sort(boxes, (o1, o2)->{
+      if (o1.e != o2.e) return o1.e - o2.e;
+      return o2.s - o1.s;
     });
-  }
-
-  static int pro(){
-    sortBoxes();
-    int served = 0;
-    int boxesIdx = 0;
-    int qSize = 0; //큐 내 현재 박스의 갯수
-    List<Box> q = new ArrayList<>();
-    for (int st = 1; st <= N; st++){ //역마다
-//      System.out.println(" ======= station : "+ st+" ==============");
-      //q정렬
-      Collections.sort(q);
-      //1. 배송할 게 있다면 배송한다
-      while ( !q.isEmpty() && q.get(0).e == st ){
-        Box box = q.remove(0);
-        served += box.count;
-        qSize -= box.count;
-
-//        System.out.println("배송 "+ box.e + " ,count =  "+ box.count);
+    int [] left = new int[N+1];
+    Arrays.fill(left, cap);
+    for (int i = 0; i< M; i++){
+      Box box = boxes[i];
+      int size = getMin(left, box);
+      answer += size;
+      for (int j = box.s; j <box.e; j++){
+        left[j] -= size;
       }
-      //2. 현재 가지고 있는 것보다 이득이거나, 빈자리가 있다면 담는다.
-      while (boxesIdx < M && boxes[boxesIdx].s == st){
-        Box cand = boxes[boxesIdx];
-//        System.out.println("승차 후보 : "+ cand);
-        if ( qSize < C ){
-//          System.out.println(" 빈공간 있음 "+ q.size()+"/"+C);
-          int possSize = Math.min(C - qSize, cand.count);
-          q.add(new Box(cand.s, cand.e, possSize));
-          qSize += possSize;
-          if(possSize != cand.count){ //남은 공간이 부족해서 다 못 넣었다면
-            cand.count -= possSize;
-          }else{
-            boxesIdx++;
-          }
-
-        }else if(q.get(q.size()-1).e > cand.e){ //이미 큐에는 가득찼다면
-//          System.out.println(" 빈공간 없음"+q.size()+"/"+C);
-
-          if(q.get(q.size()-1).e > cand.e){
-//            System.out.println(" 이득임 cand : "+ cand.e + " , 현재 bigger: "+ q.get(q.size()-1).e);
-            Box bigger = q.remove(q.size()-1);
-            if (bigger.count > cand.count){
-              q.add(q.size()-1, new Box(st, bigger.e, bigger.count - cand.count));
-              q.add(0, new Box(st, cand.e, cand.count));
-              boxesIdx++;
-            } else if (bigger.count == cand.count) {
-              q.add(0,cand);
-              boxesIdx++;
-            }else if(bigger.count < cand.count){
-              q.add(0,new Box(st, cand.e, bigger.count));
-              cand.count -= bigger.count;
-            }
-          }
-        }else {
-          boxesIdx++;
-          break;
-        }
-      }
-//      System.out.println("--현재 큐 --");
-//      System.out.println(q);
-
     }
-    return served;
-
-
+    return answer;
   }
-  public static void main(String[] args) throws IOException{
+  static int getMin(int [] left, Box box){
+    int minVal = box.count;
+    for (int i = box.s; i < box.e; i++){
+      minVal = Math.min(minVal, left[i]);
+    }
+    return minVal;
+  }
+
+
+  public static void main(String[] args) throws IOException {
     input();
-    System.out.println(pro());
+    System.out.println(sol(K, boxes));
   }
+
 }
