8a5325547909fe8c55ceb6de43f762e9762c302b
한지원 <88698607+jiwon83@users.noreply.github.com>
2023-12-12 10:02:04 +0900

    [Gold I] Title: 등차수열, Time: 560 ms, Memory: 30952 KB -BaekjoonHub

--- 백준/Gold/1994. 등차수열/등차수열.java
+++ 백준/Gold/1994. 등차수열/등차수열.java
@@ -6,44 +6,35 @@ import java.util.HashMap;
 
 class Main{
    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
    static int N;
    static int answer = 1;
    static int [][] dp;
    static int [] a;
-   static HashMap<Integer, Integer> indexOfNum;
 
    static void sol(int [] a){
       Arrays.sort(a);
-      for (int i= 0; i<N; i++) indexOfNum.put(a[i], i);
-
       dp = new int[N][N]; // dp[i][j] = i ~ j 를 마지막 등차수열로 하는 수열의 최대 길이
       for (int i = 0; i< N; i++) dp[i][i] = 1;
+      
       for (int i= 0; i<N; i++){
          for (int j = i+1; j<N; j++){
+            dp[i][j] =2;
             int d = a[j] - a[i];
             int w = i;
             while (w-1 >= 0 && a[i] - a[w-1] <= d){
-               w--;
-               if (a[i]-  a[w]== d) break;
+               if (a[i]-  a[--w]== d) break;
             }
-            if (a[i] - a[w] == d){
-               dp[i][j] = dp[w][i] + 1;
-            }else {
-               dp[i][j] =2;
-            }
+            if (a[i] - a[w] == d) dp[i][j] = Math.max(dp[i][j], dp[w][i] + 1);
             answer = Math.max(answer, dp[i][j]);
          }
       }
-//      System.out.println(Arrays.deepToString(dp));;
-
    }
    static void input() throws IOException{
       N = Integer.parseInt(br.readLine());
       a = new int[N];
-      indexOfNum = new HashMap<>();
       for (int i = 0; i<N; i++){
          a[i] = Integer.parseInt(br.readLine());
-
       }
    }
    public static void main(String[] args) throws IOException {
