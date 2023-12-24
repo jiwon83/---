7a22ba3b966d026e49902d39cadd76b7a713623c
한지원 <88698607+jiwon83@users.noreply.github.com>
2023-12-24 13:02:07 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 620 ms, Memory: 48596 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -1,119 +1,143 @@
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.io.BufferedReader;
 import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+/*
 
+ */
 public class Main {
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static StringTokenizer st;
   static StringBuilder sb = new StringBuilder();
-  static final int filterN = 5;
-  static int[] f_center = {2,2};
   static int answer;
-  static int N;
-  static int [][] dirs = {{-1,0}, {0,1}, {1,0},{0,-1}};
+  static int[][] rate;
   static int [][] map;
+  static int [][] dirs = {{-1,0}, {0,1}, {1,0},{0,-1}};
+  static int N;
+  static int mr, mc;
 
-  static void move(int N, int [][] map){
-    int inc = 1;
-    int d = 3; // <-
-    int x= N/2, y = N/2; //출발점
-    L: while (inc <= N){
-      for (int i = 0; i<2; i++){
-        for (int j = 0; j <inc; j++){
-          x = x + dirs[d][0];
-          y = y + dirs[d][1];
-          if (!inArea(x, y)) break L;
-//          System.out.println("x="+x+" y="+y);
-          int amount = map[x][y];
-          if (amount==0) continue;
-          map[x][y] = 0;
-          sandSpread(map, x, y, d, amount);
-        }
-        d = nextDir(d);
-      }
-      inc++;
-    }
-  }
-
-  static int [][] rateMap = {
-          {0, 0, 5, 0 , 0},
-          {0 , 10, 0, 10, 0},
-          {2, 7, 0, 7, 2},
-          {0 , 1, 0, 1, 0},
-          {0 , 0, 0, 0, 0}
-  };
-
-  static int nextDir(int d){
-    return d-1 < 0 ? 3:d-1;
-  }
-  static void sandSpread(int[][] map, int x, int y, int dir, int amount ){
-    int [][] spreadFilter = toSpreadFilter(rateMap, amount);
-    int [][] rotated = rotate(spreadFilter, dir, filterN);
-    int [] trans = new int[]{x-f_center[0], y-f_center[1]};
-    for (int i = 0; i < rotated.length; i++){
-      for (int j = 0; j <rotated[0].length; j++){
-        if (rotated[i][j]==0) continue;
-        int nx = i + trans[0];
-        int ny = j + trans[1];
-        if (!inArea(nx, ny)) answer += rotated[i][j];
-        else map[nx][ny] += rotated[i][j];
-      }
-    }
-  }
-  static boolean inArea(int r, int c){
-    return r >= 0 && c >= 0 && r < N && c <N;
-  }
-
-  private static int[][] rotate(int[][] spreadFilter, int dir, int filterN) {
-//    System.out.println("Main3.rotate");
-    //현재 dir = 0
-    int d = 0;
-    while (dir != d){
-      d = d + 1;
-      int [][] cp = new int[filterN][filterN];
-      for (int i = 0; i < filterN; i++){
-        for (int j = 0; j <filterN; j++){
-          int r = j;
-          int c = (filterN-1)-i;
-          cp[r][c] = spreadFilter[i][j];
-        }
-      }
-      for (int i = 0; i< filterN; i++){
-        spreadFilter[i] = cp[i].clone();
-      }
-    }
-    return spreadFilter;
-  }
-
-  private static int[][] toSpreadFilter(int[][] rateMap, int amount) {
-    int left = amount;
-    int [][] spreadFilter = new int[filterN][filterN];
-    for (int i = 0; i< filterN; i++){
-      for (int j = 0; j<filterN; j++){
-        spreadFilter[i][j] += (int) (rateMap[i][j]*((double) amount / 100));
-        left -= spreadFilter[i][j];
-      }
-    }
-    spreadFilter[1][2] = left;
-    return spreadFilter;
-  }
 
   private static void input() throws IOException{
     N = Integer.parseInt(br.readLine());
     map = new int[N][N];
-    for (int i = 0; i<N; i++){
+    mr = N/2; mc = N/2;
+    for (int i= 0; i<N; i++){
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j <N; j++){
         map[i][j] = Integer.parseInt(st.nextToken());
       }
     }
   }
-  public static void main(String[] args) throws IOException {
-    input();
-    move(N, map);
-    System.out.println(answer);
+
+  private static int sol(int mr, int mc){
+    //1. 토네이도 순회
+    //1 1 2 2 3 3 4 4 크기만큼 이동
+    // 각각 방향 반시계 방향 회전, rate도 같이 회전
+    //2. rate 순회 일정한 비율로 분배 모래가 쌓인다.
+    int dir = 0;
+    int x = mr; int y = mc;
+    for (int l = 1; l <= N; l++){
+      for (int i = 0; i <2; i++){
+
+        rotateRate(rate);
+//        printArr("회전 후 ..", rate);
+        dir = (dir - 1) < 0? 3: dir-1;
+        for (int j = 0; j < l; j++){
+          x += dirs[dir][0];
+          y += dirs[dir][1];
+          if (!inArea(x,y)) break;
+//          System.out.println(" x ="+x + " y= "+y);
+          fileSand(x, y, rate, map);
+        }
+      }
+    }
+    return answer;
   }
+
+
+  private static void printArr(String s, int[][] rate) {
+    System.out.println(s);
+    for (int i =0; i< rate.length; i++){
+      System.out.println(Arrays.toString(rate[i]));
+    }
+  }
+
+  static final int rateN = 5;
+  //2중 배열의 메모리 구조
+  static void rotateRate(int[][] rate){
+
+    int [][] tmp = new int[rateN][rateN];
+    for (int i = 0; i <rateN; i++){
+      for (int j = 0; j <rateN; j++){
+        int ni = rateN-1 - j;
+        int nj = i;
+        tmp[ni][nj] = rate[i][j];
+      }
+    }
+    for (int i = 0; i<rateN; i++){
+      rate[i] = tmp[i].clone();
+    }
+  }
+
+  static void fileSand(int r, int c, int [][] rate, int[][] map){
+//    System.out.println("fileSand + "+ map[r][c]);
+//    printArr(" -- before -- ", map);
+//    System.out.println("r = " + r + " c = "+ c);
+//    int [][] view = new int[N][N];
+
+    Point alpha= new Point(-1,-1);
+    int left = map[r][c];
+    for (int i = 0; i<rate.length; i++){
+      for (int j = 0; j <rate[0].length; j++){
+        if (rate[i][j]==0) continue;
+        int mi = i + r - 2;
+        int mj = j + c - 2;
+        if(rate[i][j] == -1){
+          alpha = new Point(mi, mj);
+          continue;
+        }
+        int file = (int)(map[r][c] * ((double)rate[i][j]/100));
+        if (!inArea(mi, mj)){
+          answer += file;
+        }else {
+          map[mi][mj] += file;
+//          view[mi][mj] = rate[i][j];
+        }
+//        System.out.println(" i = "+ i + " j = "+ j+ " =>");
+//        System.out.println(" mi = "+ mi + " mj = "+ mj+ " : "+file);
+        left -= file;
+      }
+    }
+    if (inArea(alpha.x, alpha.y)) map[alpha.x][alpha.y] += left;
+    else answer += left;
+    map[r][c]= 0;
+//    printArr("----view----", view);
+//    printArr(" -- after -- ", map);
+
+  }
+  static boolean inArea(int r, int c){
+    return r >= 0 && c >=0 && r <N && c <N;
+  }
+  static void initRate(){
+    rate = new int[][]{
+            {0,0,5,0,0},
+            {0, 10, -1, 10,0},
+            {2, 7, 0, 7, 2},
+            {0,1, 0, 1, 0},
+            {0,0,0,0,0}
+          };
+  }
+
+  public static void main(String[] args) throws IOException {
+//    System.out.println((int)(45 * 0.1));
+    input();
+    initRate();
+    int ans = sol(mr, mc);
+    System.out.println(ans);
+//    System.out.println(sol(mr, mc));
+  }
+
 }
